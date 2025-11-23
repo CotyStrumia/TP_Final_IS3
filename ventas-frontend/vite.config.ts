e3d8fa4011887ae917077bb208b9150be05d45f8
Coty Strumia <constanzastrumia28@gmail.com>
2025-11-23 12:42:16 -0300

    cambios en config

--- ventas-frontend/vite.config.ts
+++ ventas-frontend/vite.config.ts
@@ -1,7 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  // 🔥 ESTO ES LO QUE FALTA
+  preview: {
+    port: 5174,
+    host: "0.0.0.0",
+  },
+
+  server: {
+    port: 5174,
+    host: "0.0.0.0",
+  },
+
+  // 🔥🔥 LO MÁS IMPORTANTE → fallback para SPA
+  build: {
+    rollupOptions: {},
+  },
+
+  // ESTA ES LA CLAVE REAL
+  optimizeDeps: {},
 })
